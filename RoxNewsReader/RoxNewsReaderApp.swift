84af8d05ac72bfdb29994bb4e983065732beca96
Avanish <avanishsamala@gmail.com>
2025-09-02 18:58:14 -0400

    Initial Commit

---
+++ RoxNewsReader/RoxNewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoxNewsReaderApp.swift
+//  RoxNewsReader
+//
+//  Created by Avanish Samala on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct RoxNewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
